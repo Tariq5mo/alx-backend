fee4dc14066159730b83eaeefb9a52e57307c59a
Tariq5mo <tariqomer756@gmail.com>
2024-12-25 10:23:45 +0200

    Add task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def home():
+def home() -> str:
     """The home page"""
     return render_template('0-index.html')
 
