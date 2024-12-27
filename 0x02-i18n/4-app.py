aba785db8e03e8aba1b65413fd697a18df3a5472
Tariq5mo <tariqomer756@gmail.com>
2024-12-27 09:40:15 +0200

    Add task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Flask app
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """The configuration for the Flask app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """The best match for the supported languages
+
+    Returns:
+        str: The best match for the supported languages
+    """
+    lang = request.args.get('locale')
+    if lang in app.config['LANGUAGES']:
+        return lang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home():
+    """Home page
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
